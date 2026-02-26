f685fd5890ba390b042dcf74c6f281c17b0f962b
randyrt <randytsiory@gmail.com>
2026-02-26 12:40:03 +0300

    Réorganisation de la liste navBar navigation SPA

--- src/App.vue
+++ src/App.vue
@@ -9,14 +9,13 @@ import Navbar from './components/NavBar.vue';
 
 const navRoutes = [
   { path: '/', name: 'Accueil', icon: 'home' },
-  { path: '/webcup24', name: 'Webcup', icon: 'trophy' },
   { path: '/quality', name: 'Qualités', icon: 'star' },
   { path: '/skills', name: 'Compétences', icon: 'code' },
-  { path: '/services', name: 'Autres services', icon: 'briefcase' },
   { path: '/technology', name: 'Technologies', icon: 'cogs' },
+  { path: '/webcup24', name: 'Webcup', icon: 'trophy' },
   { path: '/projects', name: 'Projets', icon: 'project-diagram' },
+  { path: '/services', name: 'Autres services', icon: 'briefcase' },
   { path: '/testimoniales', name: 'Témoignages', icon: 'quote-right' },
   { path: '/contact', name: 'Contact', icon: ['far', 'envelope'] },
-
 ];
 </script>
