21c4180085a3cd7126fd840f248ee4324bc5be52
randyrt <randytsiory@gmail.com>
2026-03-31 10:36:58 +0300

    Changement de CV

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -53,7 +53,7 @@ const router = useRouter()
 const loading = ref<boolean>(true)
 
 function notify() {
-    window.open("/images/cv/randy2.pdf", "_blank")
+    window.open("/images/cv/Randy_Andriantsiory.pdf", "_blank")
 }
 
 const goToContact = () => {
