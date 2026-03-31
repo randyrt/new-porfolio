577d18e5ba8e69d8148ff76e7fae2bd155e11a09
randyrt <randytsiory@gmail.com>
2026-03-31 19:17:23 +0300

    m 4

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -53,7 +53,7 @@ const router = useRouter()
 const loading = ref<boolean>(true)
 
 function notify() {
-    window.open("/images/cv/real_cv_win.pdf", "_blank")
+    window.open("/images/cv/Randy_real_cv.pdf.pdf", "_blank")
 }
 
 const goToContact = () => {
