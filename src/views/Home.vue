800c68e26c4a97cb1c13cc518004bd0b6e54f654
randyrt <randytsiory@gmail.com>
2026-04-02 09:10:36 +0300

    Real new CV 4

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -53,7 +53,7 @@ const router = useRouter()
 const loading = ref<boolean>(true)
 
 function notify() {
-    window.open("/images/cv/final_new_cv.pdf", "_blank")
+    window.open("/images/cv/the_real_final_cv.pdf", "_blank")
 }
 
 const goToContact = () => {
