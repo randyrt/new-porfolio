b1a8c4201cd1c945d7349c81a5dfd24f5750749f
randyrt <randytsiory@gmail.com>
2026-04-05 08:53:18 +0300

    Fonctionnalité télécharger directement mon CV

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,8 @@
 <template>
   <Loading v-if="loading" message="Page d'accueil..." />
   <div v-else class="p-8 flex flex-col">
-    <AnimatedTitle text="« Le code ne dort jamais, et moi non plus tant que je n’ai pas atteint mes objectifs. »"  aos="fade-down" />
+    <AnimatedTitle text="« Le code ne dort jamais, et moi non plus tant que je n’ai pas atteint mes objectifs. »"
+      aos="fade-down" />
     <div class="flex justify-between space-x-8 items-center flex-col md:flex-row min-h-screen">
       <div class="box p-2 view" data-aos="fade-down">
         <span class="borderline"></span>
@@ -13,7 +14,9 @@
           <h1 class="text-3xl font-bold mt-6 mb-2 text-black">Bienvenue sur mon portfolio !<span
               class="text-5xl ml-2 emoji"> 😎</span></h1>
           <p class="text-lg text-gray-700 mb-4">
-            Je suis Randy, un développeur <span class="font-semibold">fullStack</span> passionné par la création d'applications web et mobile, modernes et performantes.  J'adore explorer les nouvelles technologies et relever des défis techniques.
+            Je suis Randy, un développeur <span class="font-semibold">fullStack</span> passionné par la création
+            d'applications web et mobile, modernes et performantes. J'adore explorer les nouvelles technologies et
+            relever des défis techniques.
             Cela signifique que je suis capable de mener un projet de A à Z, du design de l'interface utilisateur
             jusqu'à la gestion
             du serveur et de la base de données.
@@ -21,8 +24,9 @@
           <p class="text-lg text-gray-700">
             Dans mes projets, j’utilise régulièrement <span class="font-semibold">Vue.js</span> et <span
               class="font-semibold">TypeScript</span> pour construire des interfaces réactives, <span
-              class="font-semibold">TailwindCSS</span> pour des designs modernes en plus d'avoir une solide expérience en <span class="font-semibold"> UX et UI design</span>, et <span
-              class="font-semibold">Laravel</span> pour le backend.
+              class="font-semibold">TailwindCSS</span> pour des designs modernes en plus d'avoir une solide expérience
+            en <span class="font-semibold"> UX et UI design</span>, et <span class="font-semibold">Laravel</span> pour
+            le backend.
             Ainsi qu’avec <span class="font-semibold">Docker Engine et Docker Compose</span> pour
             containeriser et orchestrer mes applications. Côté mobile, je me familiarise avec <span
               class="font-semibold">IONIC</span> pour développer des applications cross-platform. Et j’ai également de
@@ -31,12 +35,16 @@
           </p>
           <p class="text-lg text-gray-700 mt-4">
             N'hésitez pas à parcourir mon portfolio pour découvrir mes projets, compétences et expériences.
-            Si vous avez des questions ou souhaitez collaborer, n'hésitez pas à me <span class="text-sky-700 cursor-pointer underline" @click="goToContact">contacter</span> !
+            Si vous avez des questions ou souhaitez collaborer, n'hésitez pas à me <span
+              class="text-sky-700 cursor-pointer underline" @click="goToContact">contacter</span> !
           </p>
         </div>
-        <div class="flex justify-center mt-2">
-          <button class="btn-violet inline-block text-center btn-effect-5" @click="notify">
+        <div class="flex justify-center space-x-2 mt-2">
+          <button class="btn-violet inline-block text-center btn-effect-5" @click="viewCV">
             Voir mon CV
+          </button>
+          <button class="btn-violet inline-block text-center btn-effect-5" @click="downloadCV">
+            Télécharger mon CV
           </button>
         </div>
       </div>
@@ -52,8 +60,17 @@ import { useRouter } from 'vue-router'
 const router = useRouter()
 const loading = ref<boolean>(true)
 
-function notify() {
-    window.open("/images/cv/the_real_final_cv.pdf", "_blank")
+function downloadCV() {
+  const link = document.createElement('a');
+  link.href = "/images/cv/the_real_final_cv.pdf";
+  link.download = "the_real_final_cv.pdf"; 
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+}
+
+function viewCV() {
+  window.open("/images/cv/the_real_final_cv.pdf", "_blank");
 }
 
 const goToContact = () => {
@@ -96,13 +113,19 @@ img {
   .decoration {
     font-size: 10px !important;
   }
+
   .box {
     margin-left: 40px;
     margin-bottom: 20px;
   }
-  p, span, boutton, h1 {
+
+  p,
+  span,
+  boutton,
+  h1 {
     font-size: 10px !important;
   }
+
   .click-me {
     animation: none;
   }
