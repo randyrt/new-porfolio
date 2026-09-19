b99d3de620814cd5a55ee69184b7b7be68a9de73
randyrt <randytsiory@gmail.com>
2026-09-19 21:05:37 +0300

    Optimisation CV

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -70,8 +70,8 @@ const contentVisible = ref(false)
 
 function downloadCV() {
   const link = document.createElement('a');
-  link.href = "/images/cv/Randy_real_cv.pdf";
-  link.download = "Randy_real_cv.pdf";
+  link.href = "/images/cv/Randy_new.pdf";
+  link.download = "Randy_new.pdf";
   document.body.appendChild(link);
   link.click();
   document.body.removeChild(link);
@@ -79,7 +79,7 @@ function downloadCV() {
 }
 
 function viewCV() {
-  window.open("/images/cv/Randy_real_cv.pdf", "_blank");
+  window.open("/images/cv/Randy_new.pdf", "_blank");
 }
 
 const goToContact = () => {
